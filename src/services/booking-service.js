d4d340e747ef4a402c7fb8832b96361edb64b011
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-11-04 21:05:28 +0530

    Added booking logic with transactions

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,35 @@
-module.exports = {
+const axios = require('axios');
 
+const { BookingRepository } = require('../repositories');
+const db = require('../models');
+const serverConfig = require('../config/server-config');
+const { StatusCodes } = require('http-status-codes');
+const AppError = require('../utils/errors/app-error');
+
+const bookingRepository = new BookingRepository();
+
+async function createBooking(data) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const flight = await axios.get(`${serverConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data;
+        if (data.noofSeats > flightData.totalSeats) {
+            throw new AppError('Not enough seats available', StatusCodes.BAD_REQUEST);
+        }
+        const totalBillingAmount = data.noofSeats * flightData.price;
+        const bookingPayload = { ...data, totalCost: totalBillingAmount };
+        const booking = await bookingRepository.create(bookingPayload, transaction);
+        await axios.patch(`${serverConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+            seats: data.noofSeats
+        });
+        await transaction.commit();
+        return booking;
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+}
+
+module.exports = {
+    createBooking
 }
